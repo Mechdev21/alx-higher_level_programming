1532b0db76dfa1ef053b74bc138338843adbbc0d
Orji Favour <favour@titanname.com>
2023-03-13 11:14:49 -0700

    list

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,9 +3,9 @@ def replace_in_list(my_list, idx, element):
     len1 = len(my_list)
     len2 = 0
     if (idx < 0):
-        return None
+        return my_list
     if (idx > len1):
-        return None
+        return my_list
     if my_list is not None:
         for i in my_list:
             if (len2 == idx):
