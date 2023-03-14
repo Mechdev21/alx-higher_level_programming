bc54e9a2be73bf3364891bcb15089d6cdbcf5237
Orji Favour <favour@titanname.com>
2023-03-13 22:55:13 -0700

    list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@ def element_at(my_list, idx):
     if idx < 0:
         return None
     if my_list is not None:
-        length = len(my_list)
+        length = len(my_list) - 1
         i = 0
         if idx > length:
             return None
