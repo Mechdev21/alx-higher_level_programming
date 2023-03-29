e88139aa99dab780359d8ee468dad63ec75fa345
Orji Favour <favour@titanname.com>
2023-03-29 14:09:09 -0700

    oop

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+class Square:
+    """defines atype size"""
+    def __init__(self, size=0):
+        """initializes size"""
+        self.__size = size
+
+    def area(self):
+        """Returns current square area"""
+        return self.__size**2
+        
+    @property
+    def size(self):
+        return self.__size
+    @size.setter
+    def size(self, value):
+        """size setter"""
+        self.__size = value
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
