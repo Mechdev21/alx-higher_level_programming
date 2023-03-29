690a18a0941469fe95826e03162a7915f690b832
Orji Favour <favour@titanname.com>
2023-03-29 03:36:28 -0700

    python

--- 0x03-python-data_structures/9-max_integer.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def max_integer(my_list=[]):
-    if not my_list:
-        return None
-    max_ = my_list[0]
-    for b in my_list:
-        if b > max_:
-            max_ = b
-    return max_
