059fdcf645fd93f3f06647747157f552d55996cf
Orji Favour <favour@titanname.com>
2023-03-30 04:20:34 -0700

    oop

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+
+"""size must be an integer, otherwise raise TypeErro
+if size is less than 0, raise a ValueError"""
+
+
 class Square:
     """defines a type square """
     def __init__(self, size=0):
