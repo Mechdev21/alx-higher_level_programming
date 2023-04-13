55d7f96cfc7bd2379c876b406999ff11f04713a8
Orji Favour <favour@titanname.com>
2023-04-13 03:20:34 -0700

    basegeometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Module of class BaseGeomety"""
+
+class BaseGeometry:
+
+    """ Creates a class BaseGeometry"""
+
+    def area(self):
+        """raises an exception for not handling area"""
+        raise Exception("area() is not implemented")
+
