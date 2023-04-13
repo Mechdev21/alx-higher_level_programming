cec4f4943d7eb9b0b8f4f0c91018b2597b2ad6fa
Orji Favour <favour@titanname.com>
2023-04-13 03:40:26 -0700

    validate value

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -20,6 +20,3 @@ class BaseGeometry:
             raise TypeError("<name> must be an integer")
         if value <= 0:
             raise ValueError("<name> must be greater than 0")
-        else:
-            self.name = name
-            self.value = value
