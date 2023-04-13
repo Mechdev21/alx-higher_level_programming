1b4d377af5caee5c0e4983d48e691aabcc160b42
Orji Favour <favour@titanname.com>
2023-04-13 00:57:53 -0700

    type

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
+
 """ this is a module that checks for inheritance"""
 
+
 def is_kind_of_class(obj, a_class):
+
     """ function that checks instance and inheritance"""
 
     if isinstance(obj, a_class):
