dc7ace8ec563193058fbcd6120e7d9615c54921c
Orji Favour <favour@titanname.com>
2023-04-14 01:28:09 -0700

    Rectangle

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -3,20 +3,17 @@
 """ Module of class BaseGeometry"""
 
 class BaseGeometry:
-
     """creates a class basegeometry"""
 
     def area(self):
-
         """raise an exception error"""
 
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-
         """validates the value"""
 
         if type(value) != int:
-            raise TypeError("<name> must be an integer")
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("<name> must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
