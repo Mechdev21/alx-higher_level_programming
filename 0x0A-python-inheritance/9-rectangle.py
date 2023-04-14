1a1cc44d6bcbaf1098e32ff4cabac69f461581e3
Orji Favour <favour@titanname.com>
2023-04-14 02:02:30 -0700

    Rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+""" Module Rectangule that inherits from BaseGeometry"""
+
+
+class Rectangle(BaseGeometry):
+    """ Creats a subclass Rectangle"""
+
+    def __init__(self, width, height):
+        """ A constructor that initialises width and heiht"""
+
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """ Implements the area of a rectangle"""
+
+        var = self.__width * self.__height
+        return var
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
