d8f4bab399bdf91c3385bf49ae57af5df0b92f35
Orji Favour <favour@titanname.com>
2024-01-25 09:31:43 -0800

    list

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+#python program that:
+#computes the square value of all integers of a matrix.
+
+def square_matrix_simple(matrix=[]):
+    new_matrix = [[value * 2 for value in row]for row in matrix]
+    return new_matrix
