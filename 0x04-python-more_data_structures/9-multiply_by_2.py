da1165c813184bfd1abd44b5b8dfb6beb5902941
Orji Favour <favour@titanname.com>
2024-01-27 04:55:56 -0800

    dict

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+# a funstion that:
+# returns a new dictionary with all values multiplied by 2
+
+def multiply_by_2(a_dictionary):
+    new_dict = dict(map((lambda x: (x[0], x[1]*2)), a_dictionary.items()))
+    return new_dict
