fad9bb86a2bfe9e2e2a1c8fadd7f4c9cc3324b30
Orji Favour <favour@titanname.com>
2024-01-27 05:51:45 -0800

    data

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -5,7 +5,4 @@
 # number without using any loops
 
 def multiply_list_map(my_list=[], number=0):
-    if my_list is None:
-        my_list = []
-    new_list = list(map((lambda x: x*number), my_list))
-    return new_list
+    return list(map((lambda x: x*number), my_list))
