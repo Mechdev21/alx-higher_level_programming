cc56842f500eb6b2b2a6bce8ef5bd37b3782dddf
Orji Favour <favour@titanname.com>
2024-01-27 00:41:06 -0800

    data structure

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# a function common elements that:
+# returns a set of common elements in two sets
+
+def common_elements(set_1, set_2):
+    for i in set_1:
+        for j in set_2:
+            if i == j:
+                return i
