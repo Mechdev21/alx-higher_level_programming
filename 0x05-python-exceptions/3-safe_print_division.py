bfad8f295764519f2af21405749617bf1f7301a2
Orji Favour <favour@titanname.com>
2024-02-05 12:03:05 -0800

    task

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,9 +2,8 @@
 def safe_print_division(a, b):
     try:
         result = a/b
-        return result
     except ZeroDivisionError:
-        result = 0
-        return None
+        result = None
     finally:
         print("Inside result: {}".format(result))
+        return (result)
