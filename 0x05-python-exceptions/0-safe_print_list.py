d84d6cd6d736a97a746b917db003d2d10753c4b5
Orji Favour <favour@titanname.com>
2024-02-05 03:32:14 -0800

    task

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+# safe_print_list: a function that:
+# prints x element of a list
+
+def safe_print_list(my_list=[], x=0):
+    n = 0
+    try:
+        while(x > 0 and n < len(my_list)):
+            print("{}".format(my_list[n]), end="")
+            n += 1
+            x -= 1
+    except IndexError:
+            print("This index is out of range")
+    print()
+    return n
