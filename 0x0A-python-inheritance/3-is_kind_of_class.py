c8407831b1f16924c9b64ac1ceb6932003b64ed5
Orji Favour <favour@titanname.com>
2024-02-19 12:25:05 -0800

    task

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+this module contain pone funcion that returns True if the object is an instance of,
+or if the object is an instance of a class that inherited from,
+the specified class ; otherwise False.
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    A function that returns TRUE if the object is an instance of
+    or the objected is an instance of a class that is inherited from
+    Aregs:
+        obj:
+        a_class:
+    Returns:
+        Treue or False
+    """
+    if isinstance(obj, a_class):
+        return (True)
+    else:
+        return (False)
