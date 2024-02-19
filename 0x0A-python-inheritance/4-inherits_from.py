efb12b919a15320bba9e2902827f92865aed455e
Orji Favour <favour@titanname.com>
2024-02-19 12:58:00 -0800

    task

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+A module containin one function that check nfor the instance
+of an object
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    function that returns True if the object is an instance of a class
+    that inherited (directly or indirectly) from the specified class
+    otherwise False.
+    Args:
+        obj:
+        a_class:
+    Return:
+        True or false
+    """
+    if not obj.__class__ is a_class:
+        return True
