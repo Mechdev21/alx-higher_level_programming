8cc7cdecff0967ca6983ec45d4f55ca4a30bb89e
Orji Favour <favour@titanname.com>
2024-02-19 19:53:11 -0800

    task

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-A module that creates a class called BaseGeometry
+A module with one class and two function:
+that creates a class called BaseGeometry
 """
 
 
@@ -12,7 +13,8 @@ class BaseGeometry:
 
     def area(self):
         """
-        raise an exception when called
+        raise an exception when called that area is not
+        implemented
         """
         raise Exception("area() is not implemented")
 
@@ -23,8 +25,5 @@ class BaseGeometry:
         """
         if type(value) != int:
             raise TypeError("<name> must be an integer")
-        elif value <= 0:
+        if value <= 0:
             raise ValueError("<name> must be greater than 0")
-        else:
-            self.name = name
-            self.value = value
