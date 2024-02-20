f36b1dc01a87c78c16e53ed2e3d286e75634075a
Orji Favour <favour@titanname.com>
2024-02-19 20:26:40 -0800

    task

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+"""A module class called Rectangle"""
+
+
+class Rectangle(BaseGeometry):
+    """creates a class called Rectangle"""
+
+    def __init__(self, width, height):
+        """width and height are private instance initialization"""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """implements the area of a rectangle"""
+        return self.__height * self.__width
+    def __str__(self):
+        """implemeents the magic function print"""
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
