a8da76b2799691e901920c485b2061fd60dbb1dc
Orji Favour <favour@titanname.com>
2024-02-19 20:09:55 -0800

    last

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,29 +1,19 @@
 #!/usr/bin/python3
-"""
-A module with one class and two function:
-that creates a class called BaseGeometry
-"""
 
+""" Module of class BaseGeometry"""
 
 class BaseGeometry:
-    """
-    This creates a class called BaseGeometry
-    that creates instance when called
-    """
+    """creates a class basegeometry"""
 
     def area(self):
-        """
-        raise an exception when called that area is not
-        implemented
-        """
+        """raise an exception error"""
+
         raise Exception("area() is not implemented")
 
-    def integer_validator(self, name:str, value):
-        """
-        a function that validates value and assumes
-        name is always astring
-        """
-        if type(value) is not int:
+    def integer_validator(self, name, value):
+        """validates the value"""
+
+        if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
