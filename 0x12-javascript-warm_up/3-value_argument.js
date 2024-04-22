e4a12a47978abd8f5f68906cb651493537e1a266
orji favour <favour@titanname.com>
2024-04-22 11:58:45 -0700

    process

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,12 @@
 #!/usr/bin/node
 
+let i = 2;
+
 if (process.argv.length < 3) {
   console.log('No argument');
 } else {
-  console.log(process.argv[2]);
+  while (i < process.argv.length) {
+    console.log(process.argv[i]);
+    i++;
+  }
 }
