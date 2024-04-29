a6587458554769f08f08e0e94972aa4b80c5328e
favour orji <favour@titanname.com>
2024-04-29 11:08:42 -0700

    new

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-"""
-Write a script that takes in the name of a state as
-an argument and lists all cities of that state, using
-the database hbtn_0e_4_usa
-"""
-import MySQLdb
-import sys
+"""0x0F. Python - Object-relational mapping - task 5. All cities by state"""
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cur = db.cursor()
-    query = """
-            SELECT `cities`.`name`
-            FROM `cities`
-            INNER JOIN `states`
-            ON `cities`.`state_id` = `states`.`id`
-            WHERE `states`.`name` = %s
-            """
-    cur.execute(query, (sys.argv[4],))
-    rows = cur.fetchall()
-    for row in rows:
-        print(", ".join(row), end=", ")
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
+
+    if len(sys.argv) != 5:
+        sys.exit('Use: 5-filter_cities.py <mysql username> <mysql password>'
+                 ' <database name> <state name>')
+
+    conn = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset='utf8')
+    cur = conn.cursor()
+    cur.execute("SELECT cities.name FROM cities LEFT JOIN states "
+                "ON cities.state_id = states.id WHERE states.name = %s "
+                "ORDER BY cities.id ASC", (sys.argv[4], ))
+    query_rows = cur.fetchall()
+    cities = [row[0] for row in query_rows]
+    print(', '.join(cities))
+    cur.close()
+    conn.close()
